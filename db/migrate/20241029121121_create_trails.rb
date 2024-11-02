5374d156b6c45f8dcfb377f4813084cb1594e21b
RocketRobC <rob@robcornish.net>
2024-11-02 13:47:43 +1100

    TRA-001 migration and cache clean

--- db/migrate/20241029121121_create_trails.rb
+++ db/migrate/20241029121121_create_trails.rb
@@ -1,10 +1,29 @@
 class CreateTrails < ActiveRecord::Migration[8.0]
   def change
+    create_table :trail_networks do |t|
+      t.string :name
+      t.string :address
+      t.string :city
+      t.string :postcode
+      t.string :country
+
+      t.timestamps
+    end
+
     create_table :trails do |t|
+      t.references :trail_networks
       t.string :name
       t.string :grade
-      t.timestamps
 
+      t.timestamps
+    end
+
+    create_table :trail_statuses do |t|
+      t.references :trails
+      t.string :status
+      t.text :reason
+
+      t.timestamps
     end
   end
 end
